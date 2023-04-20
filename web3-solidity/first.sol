d12029038e655b8c50e398234c3870948c42f7d7
tushar tripathi <tushartripathi@tushars-MacBook-Air.local>
2023-04-20 21:54:41 +0530

    first program

---
+++ web3-solidity/first.sol
@@ -0,0 +1,9 @@
+// SPDX-Licnese-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+contract first{
+    uint public  a=23;
+    function ffirst(uint a) public pure returns (uint){
+a =34;
+ return a;
+    }
+}
